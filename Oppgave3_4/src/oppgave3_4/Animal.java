57b81ee8e00d332b563aa160a714c4758c316891
EkstremFrukt <EkstremFrukt@users.noreply.github.com>
2019-01-25 03:15:56 +0100

    Update Animal.java

--- Oppgave3_4/src/oppgave3_4/Animal.java
+++ Oppgave3_4/src/oppgave3_4/Animal.java
@@ -12,17 +12,14 @@ public class Animal {
     public Animal () {}
 
     public Animal(Animal source) {
-        info[] kopi = new info[source.length];
         this.type = source.type;
-        this.info = new ArrayList<Info>();
+        int a = info.length;
+                
+
     }
 
     @Override
     public String toString() {
         return "Type of animal: " + type + Arrays.toString(info);
-    }
-
-    Object get(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
