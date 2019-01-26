98458c7c210c6c20e6d70c0c8ba2fa874fd43230
EkstremFrukt <EkstremFrukt@users.noreply.github.com>
2019-01-26 01:03:16 +0100

    Update Oppgave3_4.java

--- Oppgave3_4/src/oppgave3_4/Oppgave3_4.java
+++ Oppgave3_4/src/oppgave3_4/Oppgave3_4.java
@@ -3,7 +3,7 @@ package oppgave3_4;
 public class Oppgave3_4 {
 
     public static void main(String[] args) {
-        Animal dyr1 = new Animal();
+        /*Animal dyr1 = new Animal();
         dyr1.type = "Hest";
         dyr1.info = new int[2];
         dyr1.info[0] = 44;
@@ -30,5 +30,22 @@ public class Oppgave3_4 {
         //output:
         //Type of animal: Dog [10, 40], name of dog: Fido, life expectancy: 10, weight: 40 
         //Type of animal: Dog [10, 40], name of dog: Pluto, life expectancy: 10, weight: 40
+        */
+        
+        Dog peter = new Dog("Peter");
+
+        Dog kopiert = new Dog(peter);
+        System.out.println(kopiert);
+
+        kopiert.name = "Hans";
+        kopiert.info[1] = 50;
+        System.out.println(kopiert);
+        System.out.println(peter);
+        
+        Animal hund = new Animal(peter);
+        System.out.println(hund); 
+
+        // HINT: her forventer vi at toString-metoden til Dog blir kjørt fordi 
+        // det originale peter-objektet er av type Dog
     }    
 }
