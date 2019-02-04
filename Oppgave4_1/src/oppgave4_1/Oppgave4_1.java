5fc786323269fad03593d90829995abf50baccd5
EkstremFrukt <EkstremFrukt@users.noreply.github.com>
2019-02-04 01:46:35 +0100

    a

--- Oppgave4_1/src/oppgave4_1/Oppgave4_1.java
+++ Oppgave4_1/src/oppgave4_1/Oppgave4_1.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oppgave4_1;
 
-/**
- *
- * @author dette
- */
+import java.util.ArrayList;
+import static javafx.scene.input.KeyCode.T;
+
 public class Oppgave4_1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        array = new T[num];
+        
     }
     
 }
